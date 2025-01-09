1de26dc43eec268243b84719e1b25ad041d54988
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2025-01-09 10:25:16 +0100

    Modo Inivitado activado

--- app/src/main/java/com/example/wambi_shop_v2/activity_register.kt
+++ app/src/main/java/com/example/wambi_shop_v2/activity_register.kt
@@ -1,19 +1,26 @@
 package com.example.wambi_shop_v2
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class activity_register : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class ActivityRegister : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
 
-    }
+        // Configuramos el botón "Modo Invitado"
+        val modoInvitadoButton = findViewById<Button>(R.id.Modo_Invitado)
+        modoInvitadoButton.setOnClickListener {
+            // Iniciar el servicio para la cuenta regresiva de 30 segundos
+            val serviceIntent = Intent(this, GuestModeService::class.java)
+            startService(serviceIntent)
 
+            // Navegar a la actividad de HallShop
+            val intent = Intent(this, activity_hallshop::class.java)  // Aquí cambia a HallShopActivity
+            startActivity(intent)
+        }
+    }
 }
