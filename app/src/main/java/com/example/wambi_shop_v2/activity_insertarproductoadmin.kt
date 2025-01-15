47fc28dbf2c354eb3e4c021ab16fdf8fccef757a
Lucas Bernabe Fernandez <lucberfer@gmail.com>
2025-01-15 16:57:55 +0100

    Lucas 1 al Main

--- app/src/main/java/com/example/wambi_shop_v2/activity_insertarproductoadmin.kt
+++ app/src/main/java/com/example/wambi_shop_v2/activity_insertarproductoadmin.kt
@@ -5,13 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wambi_shop_v2.R.id.main
 
 class activity_insertarproductoadmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_insertarproductoadmin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
